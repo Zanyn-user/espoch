2984acff64f8b45da434502c1cc2358baa33170c
Zanyn-user <zanynuser@gmail.com>
2024-11-05 09:12:46 -0500

    espoch

---
+++ src/main/java/com/mycompany/espoch/Espoch.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.espoch;
+
+/**
+ *
+ * @author User
+ */
+public class Espoch {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
